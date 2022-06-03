b87575c8223c1859d528d00846b7dadb920386ea
Dave O'Connor <1656866+HammerMeetNail@users.noreply.github.com>
2022-06-03 15:01:51 -0400

    Add expandable tags

--- src/routes/Namespaces/Repository/Tabs/Tags/Table.tsx
+++ src/routes/Namespaces/Repository/Tabs/Tags/Table.tsx
@@ -1,6 +1,7 @@
-import { TableComposable, Thead, Tr, Th, Tbody, Td } from '@patternfly/react-table';
+import { ExpandableRowContent, TableComposable, Thead, Tr, Th, Tbody, Td } from '@patternfly/react-table';
 import { Button, ClipboardCopy, Modal, ModalVariant, Text } from '@patternfly/react-core';
 import {useState} from 'react';
+import React from 'react';
 
 export interface Tag {
     Name: string;
@@ -8,9 +9,17 @@ export interface Tag {
     Security: string;
     Size: string;
     LastModified: string;
+    Expires: string;
     Manifest: string;
     Pull: string;
     Digest: string;
+    ManifestLists?: {
+        Arch?: string;
+        Security?: string;
+        Size?: string;
+        Manifest?: string;
+        Format: 0 | 1 | 2 | 3;
+    };
 }
 
 const columnNames = {
@@ -39,12 +48,12 @@ export default function Table(props: TableProps) {
     
     const setTagSelected = (tag: Tag, isSelecting = true) =>
         setSelectedTagNames(prevSelected => {
-            const otherSelectedRepoNames = prevSelected.filter(r => r !== tag.Name);
-            return isSelecting && isTagSelectable(tag) ? [...otherSelectedRepoNames, tag.Name] : otherSelectedRepoNames;
+            const otherSelectedtagNames = prevSelected.filter(r => r !== tag.Name);
+            return isSelecting && isTagSelectable(tag) ? [...otherSelectedtagNames, tag.Name] : otherSelectedtagNames;
     });
 
-    const onSelectTag = (repo: Tag, rowIndex: number, isSelecting: boolean) => {
-        setTagSelected(repo, isSelecting);
+    const onSelectTag = (tag: Tag, rowIndex: number, isSelecting: boolean) => {
+        setTagSelected(tag, isSelecting);
         setRecentSelectedRowIndex(rowIndex);
     };
 
@@ -54,8 +63,18 @@ export default function Table(props: TableProps) {
         setIsModalOpen(!isModalOpen)
     }
 
+    // Tag expansion
+    const initialExpandedTagNames = props.tags.filter(tag => !!tag.ManifestLists).map(tag => tag.Name);
+    const [expandedTagNames, setExpandedTagNames] = useState<string[]>(initialExpandedTagNames);
+    const setTagExpanded = (tag: Tag, isExpanding = true) =>
+    setExpandedTagNames(prevExpanded => {
+      const otherExpandedtagNames = prevExpanded.filter(r => r !== tag.Name);
+      return isExpanding ? [...otherExpandedtagNames, tag.Name] : otherExpandedtagNames;
+    });
+    const isTagExpanded = (tag: Tag) => expandedTagNames.includes(tag.Name);
+
     return (
-        <>
+        <React.Fragment>
             <Modal
                 title={`Fetch Tag: ${modalImageTag}`}
                 isOpen={isModalOpen}
@@ -79,46 +98,94 @@ export default function Table(props: TableProps) {
                     docker pull quay.io/{props.organization}/{props.repository}@{modalImageDigest}
                 </ClipboardCopy>
             </Modal>
-            <TableComposable>
-                <Thead>
-                    <Tr>
-                        <Th
-                            select={{
-                            onSelect: (_event, isSelecting) => selectAllTags(isSelecting),
-                            isSelected: selectedTagNames.length === selectableTags.length
-                            }}
-                        />
-                        <Th>Tag</Th>
-                        <Th>OS</Th>
-                        <Th>Security</Th>
-                        <Th>Size</Th>
-                        <Th>Last Modified</Th>
-                        <Th>Manifest</Th>
-                        <Th>Pull</Th>
-                    </Tr>
-                </Thead>
-                <Tbody>
-                {props.tags.map((tag, rowIndex) => (
-                    <Tr key={rowIndex}>
-                        <Td
-                        select={{
-                            rowIndex,
-                            onSelect: (_event, isSelecting) => onSelectTag(tag, rowIndex, isSelecting),
-                            isSelected: selectedTagNames.includes(tag.Name),
-                        }}
-                        />
-                        <Td dataLabel={columnNames.Tag}>{tag.Name}</Td>
-                        <Td dataLabel={columnNames.OS}>{tag.OS}</Td>
-                        <Td dataLabel={columnNames.Security}>{tag.Security}</Td>    
-                        <Td dataLabel={columnNames.Size}>{tag.Size}</Td>
-                        <Td dataLabel={columnNames.LastModified}>{tag.LastModified}</Td>
-                        <Td dataLabel={columnNames.Manifest}>{tag.Manifest}</Td>
-                        <Td dataLabel={columnNames.Pull} onClick={()=>{openModal(tag)}}><i className="fa fa-download"></i></Td>
-                    </Tr>
-                ))}
-                </Tbody>
-            </TableComposable>
-        </>
+            <TableComposable aria-label="Expandable table">
+        <Thead>
+          <Tr>
+            <Th />
+            <Th
+                select={{
+                onSelect: (_event, isSelecting) => selectAllTags(isSelecting),
+                isSelected: selectedTagNames.length === selectableTags.length
+                }}
+            />
+            <Th>Tag</Th>
+            <Th>OS</Th>
+            <Th>Security</Th>
+            <Th>Size</Th>
+            <Th>Last Modified</Th>
+            <Th>Expires</Th>
+            <Th>Manifest</Th>
+            <Th>Pull</Th>
+          </Tr>
+        </Thead>
+        {props.tags.map((tag, rowIndex) => {
+          // Some arbitrary examples of how you could customize the child row based on your needs
+          const childHasNoPadding = false;
+          const archColspan = 2;
+          const securityColspan = 1;
+          const sizeColspan = 3;
+          const manifestColspan = 1;
+          return (
+            <Tbody key={tag.Name} isExpanded={isTagExpanded(tag)}>
+              <Tr>
+                <Td
+                  expand={
+                    tag.ManifestLists
+                      ? {
+                          rowIndex,
+                          isExpanded: isTagExpanded(tag),
+                          onToggle: () => setTagExpanded(tag, !isTagExpanded(tag))
+                        }
+                      : undefined
+                  }
+                />
+                <Td
+                    select={{
+                        rowIndex,
+                        onSelect: (_event, isSelecting) => onSelectTag(tag, rowIndex, isSelecting),
+                        isSelected: selectedTagNames.includes(tag.Name),
+                    }}
+                />
+                <Td dataLabel={columnNames.Tag}>{tag.Name}</Td>
+                <Td dataLabel={columnNames.OS}>{tag.OS}</Td>
+                <Td dataLabel={columnNames.Security}>{typeof(tag.ManifestLists) != 'undefined' ? "See Child Manifest" : tag.Security}</Td>
+                <Td dataLabel={columnNames.Size}>{typeof(tag.ManifestLists) != 'undefined' ? "N/A" : tag.Size}</Td>
+                <Td dataLabel={columnNames.LastModified}>{tag.LastModified}</Td>
+                <Td dataLabel={columnNames.LastModified}>{tag.Expires}</Td>
+                <Td dataLabel={columnNames.Manifest}>{tag.Manifest}</Td>
+                <Td dataLabel={columnNames.Pull} onClick={()=>{openModal(tag)}}><i className="fa fa-download"></i></Td>
+              </Tr>
+              {tag.ManifestLists ? (
+                <Tr isExpanded={isTagExpanded(tag)}>
+                  <Td />
+                  <Td />
+                  {tag.ManifestLists.Arch ? (
+                    <Td dataLabel="tag detail 1" noPadding={childHasNoPadding} colSpan={archColspan}>
+                      <ExpandableRowContent>{tag.ManifestLists.Arch}</ExpandableRowContent>
+                    </Td>
+                  ) : null}
+                  {tag.ManifestLists.Security ? (
+                    <Td dataLabel="tag detail 2" noPadding={childHasNoPadding} colSpan={securityColspan}>
+                      <ExpandableRowContent>{tag.ManifestLists.Security}</ExpandableRowContent>
+                    </Td>
+                  ) : null}
+                  {tag.ManifestLists.Size ? (
+                    <Td dataLabel="tag detail 3" noPadding={childHasNoPadding} colSpan={sizeColspan}>
+                      <ExpandableRowContent>{tag.ManifestLists.Size}</ExpandableRowContent>
+                    </Td>
+                  ) : null}
+                  {tag.ManifestLists.Manifest ? (
+                    <Td dataLabel="tag detail 3" noPadding={childHasNoPadding} colSpan={manifestColspan}>
+                      <ExpandableRowContent>{tag.ManifestLists.Manifest}</ExpandableRowContent>
+                    </Td>
+                  ) : null}
+                </Tr>
+              ) : null}
+            </Tbody>
+          );
+        })}
+      </TableComposable>
+        </React.Fragment>
     )
 }
 
